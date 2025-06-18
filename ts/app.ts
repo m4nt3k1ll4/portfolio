315d5504ac1436fef12667896bf3d2ca743afa1f
Juan <juanchin23ad@gmail.com>
2025-06-17 19:24:51 -0500

    formatted code and changes in ux

--- ts/app.ts
+++ ts/app.ts
@@ -8,24 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
         console.error("No se encontraron los elementos del formulario en el DOM.");
         return;
     }
-
     contactForm.addEventListener('submit', (event: SubmitEvent) => {
 
         event.preventDefault();
-
         const formData = new FormData(contactForm);
-
         const dataToPost = new URLSearchParams();
-
         const GOOGLE_FORM_URL = 'https://docs.google.com/forms/u/0/d/e/1FAIpQLScBzd01tfoUaV8RsqRFLXeg0UP-r-uI3ZT7N4q5kiLopsmAIQ/formResponse'; // ¡DEBES REEMPLAZAR ESTA URL DE EJEMPLO!
         const NAME_ID = 'entry.135050974';
         const EMAIL_ID = 'entry.976128578';
         const MESSAGE_ID = 'entry.1391685641';
-
         dataToPost.append(NAME_ID, formData.get('name') as string);
         dataToPost.append(EMAIL_ID, formData.get('email') as string);
         dataToPost.append(MESSAGE_ID, formData.get('message') as string);
-
 
         fetch(GOOGLE_FORM_URL, {
             method: 'POST',
